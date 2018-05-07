cc805bd9d5bcd77b70c9d580ad46b87050dc1606
nickbrenn <nbren@tutanota.com>
2018-05-07 16:56:59 -0400

    Basic files set up

--- bears/bearController.js
+++ bears/bearController.js
@@ -1,24 +1,45 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 router
-  .route('/')
+  .route("/")
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' });
+    res.status(200).json({ route: "/api/bears/" });
   })
   .post((req, res) => {
-    res.status(201).json({ status: 'please implement POST functionality' });
+    res.status(201).json({ status: "please implement POST functionality" });
   });
 
 router
-  .route('/:id')
+  .route("/:id")
   .get((req, res) => {
-    res.status(200).json({ route: '/api/bears/' + req.params.id });
+    res.status(200).json({ route: "/api/bears/" + req.params.id });
   })
   .delete((req, res) => {
-    res.status(200).json({ status: 'please implement DELETE functionality' });
+    res.status(200).json({ status: "please implement DELETE functionality" });
   })
   .put((req, res) => {
-    res.status(200).json({ status: 'please implement PUT functionality' });
+    res.status(200).json({ status: "please implement PUT functionality" });
   });
+
+function get(req, res) {
+  Bear.find().then(bears => {
+    res.status(200).json(bears);
+  });
+}
+
+function post(req, res) {
+  const bearData = req.body;
+
+  const bear = new Bear(bearData);
+
+  bear
+    .save()
+    .then(bear => {
+      res.status(201).json(bear);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const bearController = require('./bears/bearController');
+mongoose
+  .connect("mongodb://localhost/beardb")
+  .then(mongo => {
+    console.log("connected to database");
+  })
+  .catch(err => {
+    console.log("Error connecting to database", err);
+  });
+
+const bearController = require("./bears/bearController");
 
 const server = express();
 
@@ -10,11 +20,11 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.get('/', function(req, res) {
-  res.status(200).json({ api: 'running' });
+server.get("/", function(req, res) {
+  res.status(200).json({ api: "running" });
 });
 
-server.use('/api/bears', bearController);
+server.use("/api/bears", bearController);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
